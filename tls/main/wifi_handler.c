42eb7b73ccefc9f5e17a0b41a9d121f64daaf569
Mattias Kallsäby <mattias.kallsaby@creekside.se>
2024-12-13 10:21:52 +0100

    wifi config

--- tls/main/wifi_handler.c
+++ tls/main/wifi_handler.c
@@ -1,7 +1,11 @@
 #include "wifi_handler.h"
 #include "printer_helper.h"
+#include "esp_system.h"
 
 void wifi_handler_start()
 {
     PRINTFC_WIFI_HANDLER("WiFi Handler is starting");
+
+    PRINTFC_WIFI_HANDLER("Using ssid: %s%s%s", green, CONFIG_WIFI_SSID, reset);
+    PRINTFC_WIFI_HANDLER("Using password: %s%s%s", green, CONFIG_WIFI_PASSWORD, reset);
 }
